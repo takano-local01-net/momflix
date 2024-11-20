f48083573e85d6257614573ed6ff4312702c147a
takano-local01-net <takano@local01.net>
2024-11-20 23:38:27 +0900

    サインアップ・サインイン機能の実装

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,13 @@
 /* eslint-disable */
+import { useRouter } from 'next/router';
 import { useState, useCallback } from 'react';
-import Input from "@/components/input"
+import Input from "@/components/input";
+import axios from 'axios';
+import { signIn } from 'next-auth/react';
 
 const Auth = () => {
+    const router = useRouter();
+
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -12,6 +17,38 @@ const Auth = () => {
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
     }, [])
+
+
+    const login = useCallback(async () => {
+        try {
+            await signIn('credentials', {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/'
+            });
+
+            router.push('/');
+        } catch (error) {
+            console.log(error);
+        }
+
+    },[email, password]);
+
+    const register = useCallback(async () => {
+        try {
+            await axios.post('/api/register', {
+                email,
+                name,
+                password
+            });
+
+            login();
+        } catch (error) {
+            console.log(error);
+        }
+
+    },[email, name, password, login]);
 
   return (
     <div className='relative w-full h-full bg-[url(/images/hero.jpg)] bg-no-repeat bg-center bg-fixed bg-cover'>
@@ -48,7 +85,7 @@ const Auth = () => {
                             value={password}
                         />
                     </div>
-                    <button className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 tranition'>
+                    <button onClick={variant === 'login' ? login : register} className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 tranition'>
                         {variant === 'login' ? 'サインイン' : '作成'}
                     </button>
                     <p className='text-neutral-500 mt-12'>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>MomFlix</h1>
+      <h1 className="text-white">MomFlix</h1>
     </div>
   );
 }
