3a4d16cfd5f59cb653125de004413dc909ad7732
takano-local01-net <takano@local01.net>
2024-11-23 11:08:30 +0900

    インデント修正

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -53,91 +53,91 @@ const Auth = () => {
 
     },[email, name, password, login]);
 
-  return (
-    <div className='relative w-full h-full bg-[url(/images/hero.jpg)] bg-no-repeat bg-center bg-fixed bg-cover'>
-        <div className='bg-black w-full h-full lg:bg-opacity-50'>
-            <nav className='px-12 py-5'>
-                <img src='/images/logo.png' alt='Logo' className='h-12' />
-            </nav>
-            <div className='flex justify-center'>
-                <div className='bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 rounded-md w-full'>
-                    <h2 className='text-white text-4xl mb-8 font-semibold'>
-                        {variant === 'login' ? 'サインイン' : '新規アカウント作成'}
-                    </h2>
-                    <div className='flex flex-col gap-4'>
-                        {variant === 'register' && (
+    return (
+        <div className='relative w-full h-full bg-[url(/images/hero.jpg)] bg-no-repeat bg-center bg-fixed bg-cover'>
+            <div className='bg-black w-full h-full lg:bg-opacity-50'>
+                <nav className='px-12 py-5'>
+                    <img src='/images/logo.png' alt='Logo' className='h-12' />
+                </nav>
+                <div className='flex justify-center'>
+                    <div className='bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 rounded-md w-full'>
+                        <h2 className='text-white text-4xl mb-8 font-semibold'>
+                            {variant === 'login' ? 'サインイン' : '新規アカウント作成'}
+                        </h2>
+                        <div className='flex flex-col gap-4'>
+                            {variant === 'register' && (
+                                <Input 
+                                        label='ニックネーム'
+                                        onChange={(ev: any) => setName(ev.target.value)}
+                                        id='name'
+                                        value={name}
+                                />
+                            )}
                             <Input 
-                                    label='ニックネーム'
-                                    onChange={(ev: any) => setName(ev.target.value)}
-                                    id='name'
-                                    value={name}
+                                label='メールアドレス'
+                                onChange={(ev: any) => setEmail(ev.target.value)}
+                                id='email'
+                                type='email'
+                                value={email}
                             />
-                        )}
-                        <Input 
-                            label='メールアドレス'
-                            onChange={(ev: any) => setEmail(ev.target.value)}
-                            id='email'
-                            type='email'
-                            value={email}
-                        />
-                        <Input 
-                            label='パスワード'
-                            onChange={(ev: any) => setPassword(ev.target.value)}
-                            id='password'
-                            type='password'
-                            value={password}
-                        />
-                    </div>
-                    <button onClick={variant === 'login' ? login : register} className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 tranition'>
-                        {variant === 'login' ? 'サインイン' : '作成'}
-                    </button>
-                    <div className='flex flex-row items-center gap-4 mt-8 justify-center'>
-                       <div
-                            onClick={() => signIn('google', { callbackUrl: '/' })}
-                            className='
-                                w-10
-                                h-10
-                                bg-white
-                                rounded-full
-                                flex
-                                items-center
-                                justify-center
-                                cursor-pointer
-                                hover:opacity-80
-                                transition
-                            '
-                        >
-                            <FcGoogle size={30} />
+                            <Input 
+                                label='パスワード'
+                                onChange={(ev: any) => setPassword(ev.target.value)}
+                                id='password'
+                                type='password'
+                                value={password}
+                            />
                         </div>
-                        <div
-                            onClick={() => signIn('github', { callbackUrl: '/' })}
-                            className='
-                                w-10
-                                h-10
-                                bg-white
-                                rounded-full
-                                flex
-                                items-center
-                                justify-center
-                                cursor-pointer
-                                hover:opacity-80
-                                transition
-                            '
-                        >
-                            <FaGithub size={30} />
+                        <button onClick={variant === 'login' ? login : register} className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 tranition'>
+                            {variant === 'login' ? 'サインイン' : '作成'}
+                        </button>
+                        <div className='flex flex-row items-center gap-4 mt-8 justify-center'>
+                            <div
+                                onClick={() => signIn('google', { callbackUrl: '/' })}
+                                className='
+                                    w-10
+                                    h-10
+                                    bg-white
+                                    rounded-full
+                                    flex
+                                    items-center
+                                    justify-center
+                                    cursor-pointer
+                                    hover:opacity-80
+                                    transition
+                                '
+                            >
+                                <FcGoogle size={30} />
+                            </div>
+                            <div
+                                onClick={() => signIn('github', { callbackUrl: '/' })}
+                                className='
+                                    w-10
+                                    h-10
+                                    bg-white
+                                    rounded-full
+                                    flex
+                                    items-center
+                                    justify-center
+                                    cursor-pointer
+                                    hover:opacity-80
+                                    transition
+                                '
+                            >
+                                <FaGithub size={30} />
+                            </div>
                         </div>
+                        <p className='text-neutral-500 mt-12'>
+                            {variant === 'login' ? 'アカウントがない場合：' : '既にアカウントがある場合：'}
+                            <span onClick={toggleVariant} className='text-white ml-1 hover:underline cursor-pointer'>
+                                {variant === 'login' ? '新規アカウント作成' : 'サインイン'}
+                            </span>
+                        </p>
                     </div>
-                    <p className='text-neutral-500 mt-12'>
-                        {variant === 'login' ? 'アカウントがない場合：' : '既にアカウントがある場合：'}
-                        <span onClick={toggleVariant} className='text-white ml-1 hover:underline cursor-pointer'>
-                            {variant === 'login' ? '新規アカウント作成' : 'サインイン'}
-                        </span>
-                    </p>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Auth
