a7c0208d1e628d208ea1526e5f488897875284db
takano-local01-net <takano@local01.net>
2024-11-23 11:06:41 +0900

    githubアカウントおよびgoogleアカウントでの認証機能の追加

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,9 +1,12 @@
 /* eslint-disable */
-import { useRouter } from 'next/router';
-import { useState, useCallback } from 'react';
-import Input from "@/components/input";
 import axios from 'axios';
+import { useCallback, useState } from 'react';
+import Input from "@/components/input";
 import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
+
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
 
 const Auth = () => {
     const router = useRouter();
@@ -88,6 +91,42 @@ const Auth = () => {
                     <button onClick={variant === 'login' ? login : register} className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 tranition'>
                         {variant === 'login' ? 'サインイン' : '作成'}
                     </button>
+                    <div className='flex flex-row items-center gap-4 mt-8 justify-center'>
+                       <div
+                            onClick={() => signIn('google', { callbackUrl: '/' })}
+                            className='
+                                w-10
+                                h-10
+                                bg-white
+                                rounded-full
+                                flex
+                                items-center
+                                justify-center
+                                cursor-pointer
+                                hover:opacity-80
+                                transition
+                            '
+                        >
+                            <FcGoogle size={30} />
+                        </div>
+                        <div
+                            onClick={() => signIn('github', { callbackUrl: '/' })}
+                            className='
+                                w-10
+                                h-10
+                                bg-white
+                                rounded-full
+                                flex
+                                items-center
+                                justify-center
+                                cursor-pointer
+                                hover:opacity-80
+                                transition
+                            '
+                        >
+                            <FaGithub size={30} />
+                        </div>
+                    </div>
                     <p className='text-neutral-500 mt-12'>
                         {variant === 'login' ? 'アカウントがない場合：' : '既にアカウントがある場合：'}
                         <span onClick={toggleVariant} className='text-white ml-1 hover:underline cursor-pointer'>
